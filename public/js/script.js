c87742f9c013807a6843aca768551e68d2abcfab
Mahmoud Zakaria <m.zakria90@gmail.com>
2016-09-19 18:50:10 +0200

    semi-finish the intro

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,35 @@
-//direction
-function direction(){
-	var doc = document.documentElement;
-	var docLang = doc.getAttribute('lang')
-	if (docLang == 'en'){
-		doc.setAttribute('dir','ltr')
-	}else{
-		doc.setAttribute('dir','rtl')
-	}
+// //docDirection
+// function docDirection(){
+// 	var doc = document.documentElement;
+// 	var docLang = doc.getAttribute('lang')
+// 	if (docLang == 'en'){
+// 		doc.setAttribute('dir','ltr')
+// 	}else{
+// 		doc.setAttribute('dir','rtl')
+// 	}
+// }
+
+
+
+// //Calling all functions on load events
+// window.addEventListener('load',function(){
+// 	docDirection();
+// })
+
+
+//Calling all functions on other events
+
+
+
+//others
+var el = document.getElementById('intro-me');
+function zoomMe(){
+	this.classList.toggle('zoomed')
+	window.removeEventListener('mousedown',zoomMe)
 }
 
+function zoomOut(){
+	el.classList.remove('zoomed')
+}
 
-
-//Calling all functions on load events
-window.addEventListener('load',function(){
-	direction();
-
-})
+el.addEventListener('mousedown',zoomMe)
--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,35 @@
-//docDirection
-function docDirection(){
-	var doc = document.documentElement;
-	var docLang = doc.getAttribute('lang')
-	if (docLang == 'en'){
-		doc.setAttribute('dir','ltr')
-	}else{
-		doc.setAttribute('dir','rtl')
-	}
-}
+// //docDirection
+// function docDirection(){
+// 	var doc = document.documentElement;
+// 	var docLang = doc.getAttribute('lang')
+// 	if (docLang == 'en'){
+// 		doc.setAttribute('dir','ltr')
+// 	}else{
+// 		doc.setAttribute('dir','rtl')
+// 	}
+// }
 
 
 
-//Calling all functions on load events
-window.addEventListener('load',function(){
-	docDirection();
-})
+// //Calling all functions on load events
+// window.addEventListener('load',function(){
+// 	docDirection();
+// })
 
 
 //Calling all functions on other events
+
+
+
+//others
+var el = document.getElementById('intro-me');
+function zoomMe(){
+	this.classList.toggle('zoomed')
+	window.removeEventListener('mousedown',zoomMe)
+}
+
+function zoomOut(){
+	el.classList.remove('zoomed')
+}
+
+el.addEventListener('mousedown',zoomMe)
