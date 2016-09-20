0a2aa88b755d043d69c5d480f606035cea5e7d9d
Mahmoud Zakaria <m.zakria90@gmail.com>
2016-09-20 16:23:04 +0200

    working on the hire section

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,8 @@
-// //docDirection
-// function docDirection(){
-// 	var doc = document.documentElement;
-// 	var docLang = doc.getAttribute('lang')
-// 	if (docLang == 'en'){
-// 		doc.setAttribute('dir','ltr')
-// 	}else{
-// 		doc.setAttribute('dir','rtl')
-// 	}
-// }
+//onLoad
 
-
-
-// //Calling all functions on load events
-// window.addEventListener('load',function(){
-// 	docDirection();
-// })
-
-
-//Calling all functions on other events
-
+window.addEventListener('load',function(){
+	document.body.className = "loaded"
+})
 
 
 //intro-me
