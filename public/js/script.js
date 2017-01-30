060d8f00051021eb13e81cf373e2dc760d4f77ee
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-01-30 17:53:53 +0200

    added the dynamic age and exp function

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,40 @@
-function zoomMe(){this.classList.toggle("zoomed"),window.removeEventListener("mousedown",zoomMe)}function rotate(){this.parentNode.className+=" rotate"}window.location.href="#home",window.addEventListener("load",function(){document.body.className="loaded"});var el=document.getElementById("intro-me");el.addEventListener("mousedown",zoomMe);var hireBtn=document.getElementById("hire-btn");hireBtn.addEventListener("mousedown",rotate);
+//onLoad
+window.location.href = "#home"
+window.addEventListener('load',function(){
+	document.body.className = "loaded";
+	dates()
+})
+
+
+//intro-me
+var el = document.getElementById('intro-me');
+function zoomMe(){
+	this.classList.toggle('zoomed')
+	window.removeEventListener('mousedown',zoomMe)
+}
+
+el.addEventListener('mousedown',zoomMe)
+
+
+//hire-me
+var hireBtn = document.getElementById('hire-btn');
+function rotate(){
+	this.parentNode.className += " " + 'rotate'
+}
+hireBtn.addEventListener('mousedown',rotate)
+
+
+//Dates and Years
+function dates(){
+	var myAge = document.getElementById('myAge');
+	var myExp = document.getElementById('myExp');
+
+	var exp = 2013
+
+	var myBirthday = new Date("June 11, 1990")
+	var now = new Date()
+	if(now.getDate() === myBirthday.getDate() && now.getMonth() === myBirthday.getMonth()){
+		myAge.innerHTML = now.getFullYear() - myBirthday.getFullYear()
+		myExp.innerHTML = now.getFullYear() - exp
+	}
+}
